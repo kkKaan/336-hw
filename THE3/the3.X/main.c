9ba6c9951914abe42fef866b1d833b8bfd5a2598
AHMET EREN COLAK <e2587921@newinek>
2024-05-25 15:24:25 +0300

    template

---
+++ THE3/the3.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: e2587921
+ *
+ * Created on May 23, 2024, 8:55 PM
+ */
+
+
+#include <xc.h>
+#include <p18cxxx.h>
+#include <stdint.h>
+#include <stdio.h>
+
+void main(void) {
+    
+    receive
+    
+    return;
+}
